d1c08cc4568baf64961b77714d01184b37061907
danheadforcode <dan@headfocode.com>
2017-12-14 10:33:11 +0000

    aadded inspiration card image

--- src/containers/HomePage/IntroBlock.js
+++ src/containers/HomePage/IntroBlock.js
@@ -1,5 +1,6 @@
 import React from 'react';
 // import logo_dim from "../../assets/logo_dim.png";
+import Inspiration from "../assets/light_bulb.jpg";
 import {
   Link
 } from 'react-router'
@@ -15,7 +16,51 @@ class IntroBlock extends React.Component {
     return (
       <section className="homepage-introblock">
         <div className="container">
-          
+          <div className="columns">    
+            <div className="column">  
+              <div className="card">
+                <div className="card-item">
+                  <figure className="image is-3by2">
+                    <img src={Inspiration} alt="Placeholder image"/>
+                  </figure>
+                </div>
+                  <div className="card-content">
+                    <div className="media-content">
+                      <h2 className="title is-4">Inspiration</h2> 
+                        This is a collection of media that users can view and add to to come up  with the next hit!
+                        
+                    </div>
+                  </div>  
+              </div>
+            </div>  
+            <div className="column">  
+              <div className="card">
+                <div className="card-item">
+                  <figure className="image is-3by2">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                  </figure>
+                </div>
+              </div>
+            </div>  
+            <div className="column">  
+              <div className="card">
+                <div className="card-item">
+                  <figure className="image is-3by2">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                  </figure>
+                </div>
+              </div>
+            </div>  
+            <div className="column">  
+              <div className="card">
+                <div className="card-item">
+                  <figure className="image is-3by2">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image"/>
+                  </figure>
+                </div>
+              </div>
+            </div>  
+          </div>    
         </div>
       </section>
     )
