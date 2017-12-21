e194d2bef9c7c1a34e83dc6503eface82a579f80
danheadforcode <dan@headfocode.com>
2017-12-21 16:59:50 +0000

    gallery setup and photos added

--- src/containers/ProductListPage.js
+++ src/containers/ProductListPage.js
@@ -1,6 +1,7 @@
 /**
  * Component that lists all Posts
  */
+import Inspiration from "../assets/light_bulb.jpg";
 import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -9,13 +10,54 @@ class ProductListPage extends React.Component {
 
   render() {
     return(
-      <section className="productpage">
+      <section className="hero is-small">
         <div className="container">
-          <div className="card"> 
-            <div className="card-content">
-              <iframe id="mfs_html5" src="https://airbit.com/widgets/html5/?uid=213343&config=404371" width="100%" height="510" frameborder="0" scrolling="no"></iframe>  
-            </div>        
-          </div>
+          <div className="columns">
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+          </div>  
+          <div className="columns">
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+            <div className="column is-one-third">
+              <div className="card"> 
+                <div className="card-content">
+                  <img src={Inspiration}/>  
+                </div>        
+              </div>
+            </div>  
+          </div>  
         </div>
       </section>
     )
