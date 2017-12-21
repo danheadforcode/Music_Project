852fa0451982e76bed38dfc8a030ead16f26dd4f
danheadforcode <dan@headfocode.com>
2017-12-21 17:05:50 +0000

    pictures added to gallery but need to be  sized

--- src/containers/ProductListPage.js
+++ src/containers/ProductListPage.js
@@ -2,6 +2,11 @@
  * Component that lists all Posts
  */
 import Inspiration from "../assets/light_bulb.jpg";
+import Spain1 from "../assets/spain_1.jpg";
+import Spain2 from "../assets/spain_2.jpg";
+import Spain3 from "../assets/spain_3.jpg";
+import Guildford_Views from "../assets/Guilford_Views.jpg";
+import Wellington_Statue from "../assets/wellington statue.jpg";
 import React from 'react'
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
@@ -23,14 +28,14 @@ class ProductListPage extends React.Component {
             <div className="column is-one-third">
               <div className="card"> 
                 <div className="card-content">
-                  <img src={Inspiration}/>  
+                  <img src={Spain1}/>  
                 </div>        
               </div>
             </div>  
             <div className="column is-one-third">
               <div className="card"> 
                 <div className="card-content">
-                  <img src={Inspiration}/>  
+                  <img src={Spain2}/>  
                 </div>        
               </div>
             </div>  
@@ -39,21 +44,21 @@ class ProductListPage extends React.Component {
             <div className="column is-one-third">
               <div className="card"> 
                 <div className="card-content">
-                  <img src={Inspiration}/>  
+                  <img src={Spain3}/>  
                 </div>        
               </div>
             </div>  
             <div className="column is-one-third">
               <div className="card"> 
                 <div className="card-content">
-                  <img src={Inspiration}/>  
+                  <img src={Guildford_Views}/>  
                 </div>        
               </div>
             </div>  
             <div className="column is-one-third">
               <div className="card"> 
                 <div className="card-content">
-                  <img src={Inspiration}/>  
+                  <img src={Wellington_Statue}/>  
                 </div>        
               </div>
             </div>  
